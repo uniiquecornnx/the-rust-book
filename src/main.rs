74d189ea1e95abe6425927a2218429d7949fdcee
uniiquecornnx <devisha18.003gupta@gmail.com>
2025-09-15 15:27:19 +0530

    added: string to number conversion

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ fn main(){
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
     println! ("you guessed - {guess}");
 
-    match guess.cmp($secrel){
+    match guess.cmp(&secret){
         Ordering::Less => println!("Too small"),
         Ordering::Greater => println!("Too big"),
         Ordering::Equal => println!("You win"),
